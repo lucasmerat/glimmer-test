7258ff517ea4d696cf653ce67164f6af036b2eef
Dave Laird <kiwiupover@gmail.com>
2019-09-26 09:07:12 -0500

    step 9

--- tests/integration/components/login/login-form-test.js
+++ tests/integration/components/login/login-form-test.js
@@ -1,26 +1,47 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | login/login-form', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<Login::LoginForm />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.deepEqual(
+      this.element.textContent
+        .trim()
+        .replace(/\s*\n+\s*/g, '\n')
+        .split('\n'),
+        ['Login', 'Select a user', 'Testy Testerson', 'Sample McData']
+    );
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <Login::LoginForm>
-        template block text
-      </Login::LoginForm>
-    `);
+  test('after selecting a user "Sign In" button enabled', async function (assert) {
+    await render(hbs`<Login::LoginForm />`);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    let button = /** @type {HTMLInputElement} */ (find('input[type="submit"]'));
+    let select = /** @type {HTMLSelectElement} */ (find('select'));
+
+    await fillIn('select', '1');
+
+    assert.equal(select.value, '1', '<option value="1"> is currently selected');
+
+    assert.equal(button.disabled, false, 'The submit button is no longer disabled');
+
+    assert.deepEqual(
+      this.element.textContent
+      .trim()
+      .replace(/\s*\n+\s*/g, '\n')
+      .split('\n'),
+      [
+        'Login',
+        'Select a user',
+        'Testy Testerson',
+        'Sample McData',
+        'Logging in with userId: 1',
+      ],
+      'validation text now shows up');
   });
 });
